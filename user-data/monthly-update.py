2ddd121fc053c32f5ff5fc4abfbeaa8e3b9abc80
abhikjain360 <abhikjain360>
2020-03-24 18:21:09 +0530

    function to calculate bill added

--- user-data/monthly-update.py
+++ user-data/monthly-update.py
@@ -36,6 +36,26 @@ def addUsage(con, user_id, usage):
     # setting previous month's reading in prev col
     sql = f'UPDATE monthly_usage SET cur = {usage} WHERE user_id = {user_id}'
     cursorObj.execute(sql)
+
+
+# method to calculate bill accorinding to slab rates
+# as per https://cspdcl.co.in/cseb/(S(5nxew3vfma2prb13acqf2xg0))/Files/Tariff_Order_FY_2019_20_09052019.pdf , pg 236
+def calculateBill(power_used):
+    bill = 0
+    if power_used <= 40:
+        return power_used * 3.7
+    else:
+        power_used -= 40
+        bill += 40 * 3.7
+    if power_used <= 160:
+        return bill + power_used * 3.8
+    else:
+        power_used -= 160
+        bill += 160 * 3.8
+    if power_used <= 400:
+        return bill + power_used * 5.8
+    else:
+        return bill + 400 * 5.3 + (power_used - 400) * 7.35
     
 
 # main method
@@ -49,3 +69,5 @@ def main():
         user_id = int(row[0][0])
         usage = float(row[0][1])
         addUsage(con, user_id, usage)
+
+    
