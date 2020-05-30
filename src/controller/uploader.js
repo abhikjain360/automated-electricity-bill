7de672532435f921ff0b79356d6435977b69cb42
abhikjain360 <abhikjain360@gmail.com>
2020-05-30 04:22:39 +0000

    failed/success redner

--- src/controller/uploader.js
+++ src/controller/uploader.js
@@ -1,3 +1,26 @@
 exports.uploader = (req, res, next) => {
 	res.render('uploader', { title: 'uploader' })
 };
+
+// for POST method image uploading
+exports.upload = (req, res, next) => {
+	let img = req.files.img;
+	let id = req.body.id;
+	let date = Date.toISOString(Date.now());
+	let imgpath = '../images/' + id + date + '.jpg';
+	img.mv(imgpath, (err) => {
+		if (err) {
+			res.redirect('uploader/failed');
+		} else {
+			res.redirect('/upload/success');
+		}
+	});
+}
+
+exports.failed = (req, res) => {
+	res.render('uploadfailed');
+}
+
+exports.success = (req, res) => {
+	res.render('uploadsuccess');
+}
