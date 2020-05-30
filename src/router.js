85792c89b91f19ea1536460fc718a78fbaded07d
abhikjain360 <abhikjain360@gmail.com>
2020-05-30 03:07:01 +0000

    uploader added

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 let index = require('./controller/index');
 let adduser = require('./controller/adduser')
+let uploader = require('./controller/uploader')
 
 router.get('/', index.index);
 router.get('/adduser', adduser.adduser);
 router.post('/adduser', adduser.post);
 router.get('/adduser/failed', adduser.failed)
 router.get('/adduser/success', adduser.success)
+router.get('/uploader', uploader.uploader)
 
 
 module.exports = router;
