27e971e8dea00f5369340dc92801f6a108e11209
abhikjain360 <abhikjain360@gmail.com>
2020-05-31 04:59:23 +0000

    code cleanup, remove multer

--- src/router.js
+++ src/router.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const multer = require("multer");
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'images/')
-	},
-	filename: (req, file, cb) => {
-		filename = new Date().toISOString() + '.jpg';
-		cb(null, filename);
-	}
-});
-const multerUpload = multer({ storage: storage });
 
 let index = require('./controller/index');
 let adduser = require('./controller/adduser');
@@ -22,13 +11,13 @@ router.get('/', index.index);
 
 // adduser
 router.get('/adduser', adduser.adduser);
-router.post('/adduser', multerUpload.none(),adduser.post);
+router.post('/adduser', adduser.multer, adduser.post);
 router.get('/adduser/failed', adduser.failed);
 router.get('/adduser/success', adduser.success);
 
 // uploader
 router.get('/uploader', uploader.uploader);
-router.post('/uploader', multerUpload.single('imgfile'), uploader.post);
+router.post('/uploader', uploader.multer, uploader.post);
 router.get('uploader/success', uploader.success);
 router.get('uploader/failed', uploader.failed);
 
