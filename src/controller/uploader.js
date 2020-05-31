cfff2e1769192265e77dbf440ed88bac1ffe505e
abhikjain360 <abhikjain360@gmail.com>
2020-05-31 04:34:01 +0000

    save upload info to db

--- src/controller/uploader.js
+++ src/controller/uploader.js
@@ -2,16 +2,31 @@ exports.uploader = (req, res) => {
 	res.render('uploader', { title: 'uploader' })
 };
 
-// for POST method image uploading
+// for POST connection
+const mongoose = require("mongoose");
+const uploadModel = require("../dbfiles/uploader.model")
+let uploadSchema = mongoose.model('upload')
+
 exports.post = (req, res) => {
-	console.log(req.body);
-	console.log(req.file);
-}
+	let upload = new uploadSchema();
+
+	upload.id = req.body.id;
+	upload.filepath = req.file.filename;
+
+	upload.save((err, doc)=> {
+		if (err) {
+			res.redirect('upload/failed');
+		}
+		else {
+			res.redirect('upload/success');
+		}
+	});
+};
 
 exports.failed = (req, res) => {
 	res.render('uploadfailed');
-}
+};
 
 exports.success = (req, res) => {
 	res.render('uploadsuccess');
-}
+};
