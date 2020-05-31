e6efcab51b7bddf62d624accb48b5b0baef555dc
abhikjain360 <abhikjain360@gmail.com>
2020-05-31 05:29:26 +0000

    body parser needed

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const path = require("path");
-const con = require('./dbfiles/connection')
+const bodyparser = require("body-parser");
+
+// required to start mongodb conenction
+const con = require('./dbfiles/connection');
 
 // importing routes
 let router = require("./router");
 
+//initializing app
 let app = express();
 
+// use bodyparser for text input
+app.use(bodyparser.urlencoded({ extended: false }));
+
+// views folder
 app.set('views', path.join(__dirname, '/views/'));
 
+// use pug to render
 app.set('view engine', 'pug');
 
+// leave the rest to router.js
 app.use('/', router);
 
+// start server
 app.listen(3000, '0.0.0.0', () => {
 	console.log("Server start");
 });
