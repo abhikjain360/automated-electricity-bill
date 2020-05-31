89debe3984c41659251e6ff56c2bae6e46fa6a9a
abhikjain360 <abhikjain360@gmail.com>
2020-05-31 03:19:33 +0000

    db start script added

--- src/dbfiles/dbstart.sh
+++ src/dbfiles/dbstart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+### Run the script with the command: sh src/dbfiles/dbstart.sh
 mongod --dbpath /home/automated-electricity-bill/src/dbfiles/db --bind_ip 127.0.0.1 --port 3001 &
 mongo --host 127.0.0.1 --port 3001
