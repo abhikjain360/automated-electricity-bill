be926a31576db48e2ef0b104976e1f8f2310fd26
abhikjain360 <abhikjain360@gmail.com>
2020-06-20 11:19:24 +0530

    model prediction added

--- src/controller/uploader.js
+++ src/controller/uploader.js
@@ -5,20 +5,40 @@ exports.uploader = (req, res) => {
 // for POST connection
 const multer = require("multer");
 const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
+destination: (req, file, cb) => {
 		cb(null, 'images/')
 	},
-	filename: (req, file, cb) => {
+filename: (req, file, cb) => {
 		filename = new Date().toISOString() + '.jpg';
 		cb(null, filename);
 	}
 });
+
 const multerUpload = multer({ storage: storage });
 
 const mongoose = require("mongoose");
-let uploadModel = require('../dbfiles/uploader.model')
-let uploadSchema = mongoose.model('upload')
-let userSchema = mongoose.model('user')
+const exec = require('child_process').exec;
+const fs = require("fs");
+let uploadModel = require('../dbfiles/uploader.model');
+let uploadSchema = mongoose.model('upload');
+let userSchema = mongoose.model('user');
+
+readPrediction = () => {
+	fs.readFile('../prediction.txt', (err, data) => {
+		if(err) {
+			console.log(err)
+		} else {
+			exec("rm ../prediction.txt", (err, stdout, stderr) => {
+				console.log('stdout: ' + stdout);
+				console.log('stderr: ' + stderr);
+				if (err !== null) {
+					console.log('exec error: ' + err);
+				}
+			})
+			return parseInt(data);
+		}
+	})
+}
 
 exports.post = (req, res) => {
 	userSchema.findOne({ id : req.body.id }, (err, doc) => {
@@ -27,14 +47,26 @@ exports.post = (req, res) => {
 		} else {
 			let upload = new uploadSchema();
 
+			// to run shell command to execute python script
+			// which runs model for predictions
+			// and saves them on a file
+			exec("echo 'write commmand to get number output'",
+			(err, stdout, stderr) => {
+				console.log('stdout: ' + stdout);
+				console.log('stderr: ' + stderr);
+				if (err !== null) {
+					console.log('exec error: ' + err);
+				}
+			});
+
 			upload.id = req.body.id;
 			upload.filepath = req.file.filename;
+			upload.prediction = readPrediction();
 
 			upload.save((err, doc)=> {
 				if (err) {
 					res.redirect('uploader/failed');
-				}
-				else {
+				} else {
 					res.redirect('uploader/success');
 				}
 			});
