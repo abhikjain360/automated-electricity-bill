6b7d9ccdcfbbca40af4315be5230629ed49a5814
Ayush Mehar <ayushm19100@iiitnr.edu.in>
2020-06-24 22:02:53 +0530

    Add files via upload

---
+++ client/src/components/HomePage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TemporaryDrawer from "../SideBar/side-bar.component";
+const HomePage = () => (
+  <div className="home-page">
+    <TemporaryDrawer />
+  </div>
+);
+
+export default HomePage;
---
+++ client/src/components/SideBar/side-bar.component.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import shockIcon from "../../assets/shock.png";
+import sideBarIcon from "../../assets/side_bar.png";
+import "./side-bar.styles.css";
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+});
+
+export default function TemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <ListItemIcon>
+          <img src={shockIcon} alt="icon" width="40px" height="40px" />
+          <ListItemText primary="  Electricity Bill Calculator" />
+        </ListItemIcon>
+      </List>
+      <Divider />
+      <List>
+        {["Dashboard", "User Profile"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      <React.Fragment className="sidebar-container">
+        <Button className="bar-button" onClick={toggleDrawer("left", true)}>
+          <img src={sideBarIcon} alt="ico" width="40px" height="40px" />
+        </Button>
+        <Drawer
+          anchor={"left"}
+          open={state["left"]}
+          onClose={toggleDrawer("left", false)}
+        >
+          {list("left")}
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
+}
